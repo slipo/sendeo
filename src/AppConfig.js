6c188e56b8273fca407a5e7666c8db35a7f5c1af
Silas Albrecht <silas.a.a@gmail.com>
2018-02-19 21:09:38 -0500

    Start using contract on testnet!

--- src/AppConfig.js
+++ src/AppConfig.js
@@ -1,5 +1,5 @@
-export const net = 'http://35.188.208.93:5000/'
+export const net = 'http://testnet-api.wallet.cityofzion.io'
 // export const net = 'http://127.0.0.1:5000/'
-export const contractScriptHash = 'a3897b57a098024e2db509032218d48f008dcafd'
+export const contractScriptHash = '6cce32b8f81f2726386ac0cd1018c992505815cb'
 
-export const neoScanBaseUrl = 'http://35.188.208.93:5000/v2/transaction/'
+export const neoScanBaseUrl = 'https://neoscan-testnet.io/transaction/'
