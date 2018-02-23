43fb7dc384f64d464f749912dfc2319b591a4751
Luke Ollett <ollett@gmail.com>
2018-02-23 07:53:35 -0800

    Add legal.

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -18,7 +18,7 @@ class AboutPage extends Component {
               <p>Sendeo is our effort at solving this problem. It also serves as a great way for us to learn how to program and develop on the NEO
               blockchain. Something we believe whole heartedly in.</p>
               <br />
-              <p className='lead black'>Made with Love by Slipo and Luke</p>
+              <p className='lead black'>Made with Love by slipo and l0c0luke</p>
               <p>We would like to extend our deep and heart felt thanks to the NEO community and all of it's resources and talent
               and developers. We are trying to stand on the shoulders of the giants that came before us. Thank You.</p>
               <br />
@@ -26,6 +26,9 @@ class AboutPage extends Component {
               <p>Security and transparency have been our main priorities while developing Sendeo. To that end, our
               public <a href='https://github.com/slipo/sendeo' target='_blank' rel='noopener noreferrer'>Sendeo Github Repo</a> is MIT licensed and we
               invite you to review and improve the code.</p>
+              <br />
+              <p className='lead black'>Legal</p>
+              <p>This code is provided for demonstration purposes only. Use at your own risk. See the <a href='https://github.com/slipo/sendeo/blob/master/LICENSE.md' target='_blank' rel='noopener noreferrer'>license</a> to learn more.</p>
             </div>
           </div>
         </div>
